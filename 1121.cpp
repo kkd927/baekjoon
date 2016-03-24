234b104989b7659fe0b0d81c24bbba81f6a9220a
KyoungDeok Kwon <kkd927@gmail.com>
2016-03-24 13:55:53 +0900

    1121번 도형 #실패

---
+++ 1121.cpp
@@ -0,0 +1,23 @@
+/*
+ * 1121번 도형
+ * https://www.acmicpc.net/problem/1121
+ * 분류: 다이나믹 프로그래밍, 수학
+ * 상태: 실패
+ */
+
+#include <stdio.h>
+
+int N, K;
+int arr[50];
+
+int main()
+{
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+	scanf("%d", &K);
+
+
+
+	return 0;
+}
