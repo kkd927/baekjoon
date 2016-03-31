0ef8dc27108da84933fb59d5a87665a3aad185b2
KyoungDeok Kwon <kkd927@gmail.com>
2016-03-31 14:44:44 +0900

    2281번 데스노트 #실패

---
+++ 2281.cpp
@@ -0,0 +1,22 @@
+/*
+ * 2281번 데스노트
+ * https://www.acmicpc.net/problem/2281
+ *
+ * ID: kkd927
+ * Github: https://github.com/kkd927/baekjoon
+ * 상태: 실패
+ */
+
+#include <stdio.h>
+#define MAX 1000
+
+int N, M;
+int name[MAX];
+
+int main()
+{
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++)
+		scanf("%d", name[i]);
+	return 0;
+}
